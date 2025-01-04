5ca74d883312dcb0a41223f9dbaa189fe0f26cc8
Partha-Shankar <parthashankar21@gmail.com>
2025-01-04 18:27:45 +0530

    Added input handling for reference string and frame initialization in LRU

--- LRU_Page_Replacement_Algorithm.c
+++ LRU_Page_Replacement_Algorithm.c
@@ -10,5 +10,22 @@ int lruvictim()
 
 void main() 
 { 
+        printf("\n\t\t\t LRU PAGE REPLACEMENT ALGORITHM");
+    printf("\n Enter no.of Frames.. ."); 
+    scanf("%d", &nof); 
+    printf(" Enter no.of reference string.."); 
+    scanf("%d", &nor); 
+    printf("\n Enter reference string.."); 
+    for (i = 0; i < nor; i++) 
+        scanf("%d", &ref[i]); 
+
+    for (i = 1; i <= nof; i++) 
+    { 
+        frm[i] = -1; 
+        lrucal[i] = 0; 
+    } 
+    for (i = 0; i < 10; i++) 
+        recent[i] = 0;
+
     return 0; 
 }
