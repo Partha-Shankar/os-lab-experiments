7b1a5d565af6b1571d3085c1111adfe5b82c4f3c
Partha-Shankar <parthashankar21@gmail.com>
2025-01-04 18:26:41 +0530

    Added basic structure for LRU page replacement algorithm with placeholder functions

---
+++ LRU_Page_Replacement_Algorithm.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int i, j, nof, nor, flag = 0, ref[50], frm[50], pf = 0, victim = -1;
+int recent[10], lrucal[50], count = 0;
+
+int lruvictim() 
+{ 
+    return 0;
+}
+
+void main() 
+{ 
+    return 0; 
+}
