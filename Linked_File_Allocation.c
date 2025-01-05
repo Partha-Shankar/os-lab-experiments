442f6de1360e12e4b1e56d97fe67c40fd02d99a9
Partha-Shankar <parthashankar21@gmail.com>
2025-01-05 12:09:06 +0530

    Added validation for user input in linked allocation.

--- Linked_File_Allocation.c
+++ Linked_File_Allocation.c
@@ -11,16 +11,29 @@ void main() {
     printf("Enter blocks already allocated: ");
     for (i = 0; i < p; i++) {
         scanf("%d", &a);
-        f[a] = 1;
+        if (a < 0 || a >= 50) {
+            printf("Error: Block number %d is out of range. Try again.\n", a);
+            i--; 
+        } else {
+            f[a] = 1;
+        }
     }
 
 x:
     printf("Enter index starting block and length: ");
     scanf("%d%d", &st, &len);
-    k = len;
+    if (st < 0 || st >= 50 || len <= 0) {
+        printf("Error: Invalid starting block or length. Try again.\n");
+        goto x;
+    }
 
+    k = len;
     if (f[st] == 0) {
         for (j = st; j < (st + k); j++) {
+            if (j >= 50) {
+                printf("Error: Block number %d is out of range.\n", j);
+                break;
+            }
             if (f[j] == 0) {
                 f[j] = 1;
                 printf("%d------->%d\n", j, f[j]);
