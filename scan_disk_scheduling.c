300b113a9b44c0b4d88ba9331f9754dfc219ca90
Partha-Shankar <parthashankar21@gmail.com>
2025-01-06 19:26:10 +0530

    Implemented sorting and separation of requests into uptrack and downtrack

--- scan_disk_scheduling.c
+++ scan_disk_scheduling.c
@@ -6,6 +6,49 @@ int SIZE, pre, head;
 int dist(int a, int b) {
     return (a > b) ? a - b : b - a;
 }
+#include <stdio.h>
+
+int request[50];
+int SIZE, pre, head, uptrack, downtrack;
+
+struct max {
+    int up, down;
+} kate[50];
+
+int dist(int a, int b) {
+    return (a > b) ? a - b : b - a;
+}
+
+void sort(int n) {
+    int i, j;
+
+    // Bubble sort to arrange requests in ascending order
+    for (i = 0; i < n - 1; i++) {
+        for (j = 0; j < n - i - 1; j++) {
+            if (request[j] > request[j + 1]) {
+                int temp = request[j];
+                request[j] = request[j + 1];
+                request[j + 1] = temp;
+            }
+        }
+    }
+
+    // Categorize requests into downtrack and uptrack
+    j = 0;
+    i = 0;
+    while (request[i] != head) {
+        kate[j++].down = request[i++];
+    }
+    downtrack = j;
+
+    i++;
+    j = 0;
+    while (i < n) {
+        kate[j++].up = request[i++];
+    }
+    uptrack = j;
+}
+
 
 int main() {
     int n, i;
